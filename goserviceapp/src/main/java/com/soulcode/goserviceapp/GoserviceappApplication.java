eeeb362a28eb1ec7e9aa3dce2d6c5a9086732a3b
N1ck49871 <nickalves0498@outlook.com>
2023-08-18 09:17:58 -0300

    Configurações iniciaos de segurança

--- goserviceapp/src/main/java/com/soulcode/goserviceapp/GoserviceappApplication.java
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/GoserviceappApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GoserviceappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GoserviceappApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GoserviceappApplication.class, args);
+    }
 }
